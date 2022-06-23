b012dee8b3861b9658d5061b990234dda2f73c6d
chuberacroi412 <lamanhtuan1299@gmail.com>
2022-06-23 23:48:52 +0700

    get live data, more chart

--- stock-prediction.py
+++ stock-prediction.py
@@ -13,7 +13,7 @@ import numpy as np
 import pandas_datareader as pdr
 import yfinance as yf
 import matplotlib.pyplot as plt
-import random 
+import math 
 # app = dash.Dash()
 # server = app.server
 # scaler=MinMaxScaler(feature_range=(0,1))
@@ -267,7 +267,7 @@ import random
 # symbol = yf.Ticker('BTC-USD')
 # df = pd.read_csv('./NSE-TATA.csv')
 # df = df.iloc[::-1]
-df = yf.download(tickers='BTC-USD', period='1d', interval='1m')
+df = yf.download(tickers='BTC-USD', period='2d', interval='1m')
 
 # df['Date'] = pd.to_datetime(df['Date'])
 df['20wma'] = df['Close'].rolling(window=140).mean()
@@ -294,7 +294,7 @@ fig = go.Figure(data = [go.Candlestick(x=df.index, open=df['Open'], high=df['Hig
 fig.add_trace(go.Scatter(x=df.index, y =df['Close'], line=dict(color='blue'), name='Close'))
 
 # fig - moving avg
-# fig.add_trace(go.Scatter(x=df['Date'], y = df['20wma'], line=dict(color='yellow'), name='20 Week moving avg'))
+fig.add_trace(go.Scatter(x=df.index, y = df['20wma'], line=dict(color='yellow'), name='20 Week moving avg'))
 
 # upper bolling band
 fig.add_trace(go.Scatter(x=df.index, y=df['20wma'] + (df['std'] * 2), line_color = 'gray', name='upper band', opacity=0.5))
@@ -312,9 +312,72 @@ fig2 = go.Figure(data = [go.Scatter(x=df.index, y=rsi)])
 fig2.add_hline(y=30, line_dash='dash', line_color='green')
 fig2.add_hline(y=70, line_dash='dash', line_color='red')
 
-fig3 = go.Figure(data = [go.Scatter(x=df.index, y=df['20wma'] + (df['std'] * 2))])
-fig4 = go.Figure(data = [go.Scatter(x=df.index, y=df['20wma'] - (df['std'] * 2))])
+# prediction
+# Analyze data
+df["Date"]=pd.to_datetime(df.index,format="%Y-%m-%d")
+df.index=df['Date']
 
+plt.figure(figsize=(16,8))
+plt.plot(df["Close"],label='Close Price history')
+
+# Sort and filter
+data=df.sort_index(ascending=True,axis=0)
+new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
+
+for i in range(0,len(data)):
+    new_dataset["Date"][i]=data['Date'][i]
+    new_dataset["Close"][i]=data["Close"][i]
+
+# Normalize
+
+scaler=MinMaxScaler(feature_range=(0,1))
+final_dataset=new_dataset.values
+train_data=final_dataset[0:987,:]
+valid_data=final_dataset[987:,:]
+
+new_dataset.index=new_dataset['Date']
+new_dataset.drop('Date',axis=1,inplace=True)
+scaler=MinMaxScaler(feature_range=(0,1))
+
+scaled_data=scaler.fit_transform(new_dataset.values)
+
+x_train_data,y_train_data=[],[]
+for i in range(60,len(train_data)):
+    x_train_data.append(scaled_data[i-60:i,0])
+    y_train_data.append(scaled_data[i,0])
+    
+x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
+x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
+
+# build model
+lstm_model=load_model("saved_model_3.h5")
+inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
+inputs_data=inputs_data.reshape(-1,1)
+inputs_data=scaler.transform(inputs_data)
+
+X_test=[]
+print(inputs_data.shape[0])
+for i in range(60,inputs_data.shape[0]):
+    X_test.append(inputs_data[i-60:i,0])
+
+X_test=np.array(X_test)
+X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+predicted_closing_price=lstm_model.predict(X_test)
+predicted_closing_price=scaler.inverse_transform(predicted_closing_price)
+
+valid_data=new_dataset[987:]
+valid_data['Predictions']=predicted_closing_price
+# print(valid_data)
+# plt.plot(valid_data[['Close',"Predictions"]])
+# plt.show()
+
+# fig for prediction
+df['Predictions'] = pd.Series(valid_data['Predictions'], index=valid_data.index)
+fig.add_trace(go.Scatter(x=df.index, y =df['Predictions'], line=dict(color='red'), name='prediction'))
+
+fig3 = go.Figure(data = [go.Scatter(x=df.index, y =df['Close'], line=dict(color='blue'), name='Close')])
+fig3.add_trace(go.Scatter(x=df.index, y =df['Predictions'], line=dict(color='red'), name='prediction'))
+# end
 
 
 # display
@@ -336,26 +399,91 @@ app.layout = html.Div([
         id='rsi_chart',
         children = [
                 html.H2("RSI chart",style={"textAlign": "center"}),
-                dcc.Graph(
+                dcc.Graph(id='rsi_real_chart',
                 figure=fig2,
                 animate=True
             )
         ]
     ),
-    dcc.Interval(id='update_rsi_chart',interval=1000*1*60)
+    dcc.Interval(id='update_rsi_chart',interval=1000*1*60),
+    dcc.Graph( 
+        figure = fig3
+    )
 ])
 
 @app.callback(Output('stock_chart', 'figure'),
-              [Input('update-live','n_intervals'),
+              [Input('update_stock_chart','n_intervals'),
               Input('select-property', 'value'), 
               Input('select-price', 'value')])
 
 def update_graph(n_interval, selected_dropdown, selected_price):
 
-    data = yf.download(tickers='BTC-USD', period='1d', interval='1m')
-    last_row = data.iloc[-1:]
-    df.append(last_row, ignore_index = True)
-    print(df)
+    df = yf.download(tickers='BTC-USD', period='2d', interval='1m')
+    # last_row = data.iloc[-1:]
+    # df.append(last_row, ignore_index = True)
+    
+    df['20wma'] = df['Close'].rolling(window=140).mean()
+    df['std'] = df['Close'].rolling(window=140).mean()
+
+    # prediction
+    df["Date"]=pd.to_datetime(df.index,format="%Y-%m-%d")
+    df.index=df['Date']
+
+    plt.figure(figsize=(16,8))
+    plt.plot(df["Close"],label='Close Price history')
+
+    # Sort and filter
+    data=df.sort_index(ascending=True,axis=0)
+    new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
+
+    for i in range(0,len(data)):
+        new_dataset["Date"][i]=data['Date'][i]
+        new_dataset["Close"][i]=data["Close"][i]
+
+    # Normalize
+
+    scaler=MinMaxScaler(feature_range=(0,1))
+    final_dataset=new_dataset.values
+    train_data=final_dataset[0:987,:]
+    valid_data=final_dataset[987:,:]
+
+    new_dataset.index=new_dataset['Date']
+    new_dataset.drop('Date',axis=1,inplace=True)
+    scaler=MinMaxScaler(feature_range=(0,1))
+
+    scaled_data=scaler.fit_transform(new_dataset.values)
+
+    x_train_data,y_train_data=[],[]
+    for i in range(60,len(train_data)):
+        x_train_data.append(scaled_data[i-60:i,0])
+        y_train_data.append(scaled_data[i,0])
+        
+    x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
+    x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
+
+    # build model
+    lstm_model=load_model("saved_model_3.h5")
+    inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
+    inputs_data=inputs_data.reshape(-1,1)
+    inputs_data=scaler.transform(inputs_data)
+
+    X_test=[]
+    print(inputs_data.shape[0])
+    for i in range(60,inputs_data.shape[0]):
+        X_test.append(inputs_data[i-60:i,0])
+
+    X_test=np.array(X_test)
+    X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+    predicted_closing_price=lstm_model.predict(X_test)
+    predicted_closing_price=scaler.inverse_transform(predicted_closing_price)
+
+    valid_data=new_dataset[987:]
+    valid_data['Predictions']=predicted_closing_price
+
+    # fig for prediction
+    df['Predictions'] = pd.Series(valid_data['Predictions'], index=valid_data.index)
+
+    # display
     
     fig = go.Figure(data = [go.Candlestick(x=df.index, open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])])
 
@@ -371,10 +499,22 @@ def update_graph(n_interval, selected_dropdown, selected_price):
         # lower bolling band
         fig.add_trace(go.Scatter(x=df.index, y=df['20wma'] - (df['std'] * 2), line_color = 'gray', fill = 'tonexty', name='lower band', opacity=0.5))
 
+    fig.add_trace(go.Scatter(x=df.index, y =df['Predictions'], line=dict(color='red'), name='prediction'))
     fig.update_layout(xaxis=dict(range=[min(df.index),max(df.index)]),
                                                 yaxis=dict(range=[min(df['Low']),max(df['High'])]))
 
+
+
     return fig
+
+@app.callback(Output('rsi_chart', 'style'),
+              [Input('select-property', 'value')])
+
+def update_graph(selected_dropdown):
+    if 'RSI' in selected_dropdown:
+        return {'display':'block'}
+
+    return {'display':'none'}
 
 if __name__=='__main__':
     app.run_server(debug=True)
